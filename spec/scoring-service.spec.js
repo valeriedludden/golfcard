b302c838e630d4300329fa64759f830dc431c005
Valerie <42677050+valeriedludden@users.noreply.github.com>
2018-10-29 10:38:17 -0600

    after class

--- spec/scoring-service.spec.js
+++ spec/scoring-service.spec.js
@@ -1,6 +1,9 @@
+let ScoringService = require("../js/js"); // get information from js file
+
+
 describe('ScoringService', () =>{
 
-    let scoringService = new ScoringService();
+    let scoringService;
     beforeEach(() => {
         scoringService = new ScoringService;
     });
@@ -9,16 +12,30 @@ describe('ScoringService', () =>{
 
 
         it('calculates score under par score correctly', () =>{
-            expect(true).toBe(true);
+            let par = 72;
+            let playerScore = Array(18).fill(3, 0, 18); //make an array with 18 slots and fill all of them with 3
+            let scoreRelativeToPar = scoringService.calculateScoreRelativeToPar(par, playerScore);
+            expect(scoreRelativeToPar).toEqual(-18);
+
         });
         it('calculates score over par score correctly', () =>{
+            let par = 72;
+            let playerScore = Array(18).fill(5, 0, 18); //make an array with 18 slots and fill all of them with 3
+            let scoreRelativeToPar = scoringService.calculateScoreRelativeToPar(par, playerScore);
+            expect(scoreRelativeToPar).toEqual(18);
 
         });
         it('returns a number', () => {
-
+            let par = 72;
+            let playerScore = Array(18).fill(3, 0, 18); //make an array with 18 slots and fill all of them with 3
+            let scoreRelativeToPar = scoringService.calculateScoreRelativeToPar(par, playerScore);
+            expect(typeof scoreRelativeToPar).toEqual('number');
         });
         it('calculates even when no scores', () => {
-
+            let par = 72;
+            let playerScore = Array(18); //make an array with 18 slots and fill all of them with 3
+            let scoreRelativeToPar = scoringService.calculateScoreRelativeToPar(par, playerScore);
+            expect(scoreRelativeToPar).toEqual(-72);
         });
 
     });
