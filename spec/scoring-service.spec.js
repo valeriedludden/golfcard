136ed074ad36d3db3d1b9df4f2e96285f4174ca2
Valerie <42677050+valeriedludden@users.noreply.github.com>
2018-10-29 09:56:45 -0600

    test

---
+++ spec/scoring-service.spec.js
@@ -0,0 +1,41 @@
+describe('ScoringService', () =>{
+
+    let scoringService = new ScoringService();
+    beforeEach(() => {
+        scoringService = new ScoringService;
+    });
+
+    describe('calculateScoreRelativeToPar', () =>{
+
+
+        it('calculates score under par score correctly', () =>{
+            expect(true).toBe(true);
+        });
+        it('calculates score over par score correctly', () =>{
+
+        });
+        it('returns a number', () => {
+
+        });
+        it('calculates even when no scores', () => {
+
+        });
+
+    });
+    describe('calculateOutScore', () => {
+        it('returns a number', () => {
+
+        });
+    });
+    describe('calculateInScore', () => {
+        it('returns a number', () => {
+
+        });
+    });
+    describe('calculateTotalScore', () => {
+        it('returns a number', () => {
+
+        });
+    });
+
+})
