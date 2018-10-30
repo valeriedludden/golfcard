809df579f1e101a36cd1b204b04685d32f7d5ab1
Valerie <42677050+valeriedludden@users.noreply.github.com>
2018-10-30 10:10:14 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# golf-testing
+# golf
--- js/js.js
+++
@@ -1,7 +0,0 @@
-
-// lets you export information to your testing file
-module.exports = class ScoringService{
-    calculateScoreRelativeToPar(par, scores){
-        return scores.map((score) => (score ? score : 0)).reduce((a, b) => a + b, 0) - par;
-    }
-};
